f27e57e386bafef43c8fbadb93a78f3a6262c7d1
Jonathan Martel <jonathmartel@gmail.com>
2020-09-21 17:30:14 -0400

    test dev

---
+++ chaine.js
@@ -0,0 +1,26 @@
+//1.  Fonction qui retire tous les espaces d'une chaine de caractère passée en paramètre et retourne la nouvelle chaine
+function retireEspaces(chaine)
+{
+    return chaine.split(" ").join("");
+}
+
+
+//2. Fonction qui efface les espaces de fin et de début et retire tous les espaces multiples d’une chaine de caractère passée en paramètre. La fonction retourne la nouvelle chaine (Vous ne pouvez pas utiliser trim()).
+function nettoieEspaces(chaine)
+{
+}
+//3.  Fonction retourne le nombre de mot d'une chaine de caractère passée en paramètre.
+function compteMots(chaine)
+{
+}
+//4. Écrire une fonction qui inverse les mots d'une chaine de caractère passée en paramètre. La fonction retourne la nouvelle chaine. La chaine ‘un deux trois’ devient ‘trois deux un’
+function inverseMots(chaine)
+{
+}
+
+//5. Fonction qui inverse les lettres des mots d'une chaine de caractère passée en paramètre. La fonction retourne la nouvelle chaine. La chaine ‘un deux trois’ devient ‘nu xued siort’
+function inverseLettres(chaine)
+{
+}
+
+module.exports = {retireEspaces, nettoieEspaces, compteMots, inverseMots, inverseLettres};
